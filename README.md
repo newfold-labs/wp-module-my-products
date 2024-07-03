3ff10380ae5a140ecdddd11d6cb82234895e6e29
Vara <varaprasadmaruboina@gmail.com>
2024-07-03 14:36:14 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+<a href="https://newfold.com/" target="_blank">
+    <img src="https://newfold.com/content/experience-fragments/newfold/site-header/master/_jcr_content/root/header/logo.coreimg.svg/1621395071423/newfold-digital.svg" alt="Newfold Logo" title="Newfold Digital" align="right" 
+height="42" />
+</a>
+
+# WordPress Products Module
+
+A module for rendering user-specific product data utilizing the Hiive Users API, which integrates with the Fulfilment Gateway API.
---
+++ includes/Products.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace NewfoldLabs\WP\Module\Products;
+
+use NewfoldLabs\WP\ModuleLoader\Container;
+use NewfoldLabs\WP\Module\Products\HiiveConnection;
+
+/**
+ * Class for handling the initialization of the marketplace module.
+ */
+class Products {
+
+	/**
+	 * Dependency injection container.
+	 *
+	 * @var Container
+	 */
+	protected $container;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param Container $container The plugin container.
+	 */
+	public function __construct( Container $container ) {
+
+		$this->container = $container;
+
+		// Module functionality goes here
+		add_action( 'rest_api_init', array( self::class, 'init_product_apis' ) );
+	}
+
+	/**
+	 * Initialize the Product API Controller.
+	 */
+	public static function init_product_apis(): void {
+		// Create a HiiveConnection
+		$hiive = new HiiveConnection();
+
+		$products_api = new ProductsApi( $hiive );
+		$products_api->register_routes();
+	}
+}
