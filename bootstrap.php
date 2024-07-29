5807f4c7d7484c1a0611601c5ee6dd500fed25d0
Brian Henry <BrianHenryIE@gmail.com>
2024-07-29 13:42:07 -0700

    Define `NFD_DATA_MODULE_MY_PRODUCTS` and check for it

--- bootstrap.php
+++ bootstrap.php
@@ -8,6 +8,13 @@ if (!defined('ABSPATH')) {
     return;
 }
 
+// Do not allow multiple copies of the module to be active
+if ( defined( 'NFD_DATA_MODULE_MY_PRODUCTS' ) ) {
+	return;
+}
+
+define( 'NFD_DATA_MODULE_MY_PRODUCTS', '1.0.0' );
+
 add_filter(
     'newfold/features/filter/register',
     function ( $features ) {
