1357f0160e819a0af104d167eeb795257a2cf38d
Brian Henry <BrianHenryIE@gmail.com>
2024-07-29 13:41:06 -0700

    Guard using `ABSPATH` `return` rather than `function_exists()`

--- bootstrap.php
+++ bootstrap.php
@@ -4,11 +4,14 @@ namespace NewfoldLabs\WP\Module\MyProducts;
 
 use function NewfoldLabs\WP\ModuleLoader\register;
 
-if ( function_exists( 'add_filter' ) ) {
-	add_filter(
-		'newfold/features/filter/register',
-		function ( $features ) {
-			return array_merge( $features, array( MyProductsFeature::class ) );
-		}
-	);
+if (!defined('ABSPATH')) {
+    return;
 }
+
+add_filter(
+    'newfold/features/filter/register',
+    function ( $features ) {
+        return array_merge( $features, array( MyProductsFeature::class ) );
+    }
+);
+
